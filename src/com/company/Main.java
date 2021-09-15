3aa68480fa345c4318136afbd1ab489a6288dec8
GodeGamleJacob <89967283+GodeGamleJacob@users.noreply.github.com>
2021-09-15 09:37:31 +0200

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,33 +3,33 @@ package com.company;
 public class Main {
 
         public static void main (String[]args){         //
-            boolean flag1 = isHoopy(202);            //1+5
-            boolean flag2 = isFrabjuous(202);        //6+10
-            System.out.println(flag1);                  //11
-            System.out.println(flag2);                  //12
-            if (flag1 && flag2) {                       //13
-                System.out.println("ping!");            //14
+            boolean flag1 = isHoopy(202);            //1+5 = opstart og return, og sender videre nedad
+            boolean flag2 = isFrabjuous(202);        //6+10 = ny start efter return, og return og videresending
+            System.out.println(flag1);                  //11 = souter "true"
+            System.out.println(flag2);                  //12 = souter "true"
+            if (flag1 && flag2) {                       //13 = hvis de to "flags" er kørt, da iværksættes
+                System.out.println("ping!");            //14 = souter "ping!"
             }                                           //
-            if (flag1 || flag2) {                       //15
-                System.out.println("pong!");            //16
+            if (flag1 || flag2) {                       //15 = hvis "flags" er kørt, da iværksættes
+                System.out.println("pong!");            //16 = souter "pong!"
             }                                           //
-        }                                               //17
+        }                                               //17 = programmet afsluttes
         public static boolean isHoopy ( int x){         //
             boolean hoopyFlag;                          //
-            if (x % 2 == 0) {                           //2
-                hoopyFlag = true;                       //3
+            if (x % 2 == 0) {                           //2 = hvis resultat, er da sandt
+                hoopyFlag = true;                       //3 = resultatet er sandt, så der kan skrives "true"
             } else {                                    //
                 hoopyFlag = false;                      //
             }                                           //
-            return hoopyFlag;                           //4
+            return hoopyFlag;                           //4 = beder om at returnere til toppen
         }                                               //
         public static boolean isFrabjuous ( int x){     //
             boolean frabjuousFlag;                      //
-            if (x > 0) {                                //7
-                frabjuousFlag = true;                   //8
+            if (x > 0) {                                //7 = hvis resultat, er da sandt
+                frabjuousFlag = true;                   //8 = resultatet er sandt, så der kan skrives "true"
             } else {                                    //
                 frabjuousFlag = false;                  //
             }                                           //
-            return frabjuousFlag;                       //9
+            return frabjuousFlag;                       //9 = beder om at returnere til toppen
         }                                               //
     }                                                   //
